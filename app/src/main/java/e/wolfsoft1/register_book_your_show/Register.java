bafa64be9625d76de46536cfafe93cd02b356baf
Rushabh Patel <wsdesignuiux@gmail.com>
2018-11-21 15:35:48 +0530

    Initial commit

---
+++ app/src/main/java/e/wolfsoft1/register_book_your_show/Register.java
@@ -0,0 +1,13 @@
+package e.wolfsoft1.register_book_your_show;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
